b7b398619fc9bc595d11957267cd60c61b55aa41
kascalho <caio.lopes@aluno.faculdadeimpacta.com.br>
2020-10-02 23:59:28 -0300

    ac3

--- t8.py
+++ t8.py
@@ -6,12 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def nao_entre_em_panico():
+    limite = 100
+    c = 1
+    p = 1
+    numero = 3
 
+    primos = "2,"
 
-    primos = "Tudo vai dar certo caros alunos!"
-
-
+    while p < limite:
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+            if (ehprimo):
+                primos = primos + str(numero) + ","
+                p += 1
+                if(p % 10 == 0):
+                    primos = primos + "<br>"
+            numero+=1
     return primos
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
